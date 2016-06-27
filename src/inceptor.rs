022fc8c873bdf33a417d6ca938b8028baf1793e6
Nerijus Arlauskas <nercury@gmail.com>
2016-06-28 01:06:55 +0300

    Basic inceptor tests.

--- src/inceptor.rs
+++ src/inceptor.rs
@@ -13,31 +13,6 @@ pub struct Inceptor<T1, T2> {
     constructor: Arc<Fn(&mut T1, &mut T2) -> Result<Option<Box<Any>>> + Send + Sync>,
 }
 
-unsafe impl<T1, T2> Send for Inceptor<T1, T2> {}
-unsafe impl<T1, T2> Sync for Inceptor<T1, T2> {}
-
-fn insert_into_vec<T>(data: &mut Vec<Option<Arc<Mutex<T>>>>,
-                      value: Arc<Mutex<T>>,
-                      used_size: &mut usize)
-                      -> usize {
-    for (i, item) in data.iter_mut().enumerate() {
-        if item.is_none() {
-            let mut tmp = Some(value);
-            mem::swap(&mut tmp, item);
-            if i + 1 > *used_size {
-                *used_size = i + 1;
-            }
-            return i;
-        }
-    }
-    let index = data.len();
-    data.push(Some(value));
-    if data.len() > *used_size {
-        *used_size = data.len();
-    }
-    index
-}
-
 impl<T1: Any, T2: Any> Inceptor<T1, T2> {
     pub fn new<F: 'static>(constructor: F) -> Inceptor<T1, T2>
         where F: Fn(&mut T1, &mut T2) -> Result<Option<Box<Any>>> + Send + Sync
@@ -98,25 +73,61 @@ impl<T1: Any, T2: Any> Inceptor<T1, T2> {
     }
 
     pub fn destroy_1(&mut self, id: usize) {
-        if id + 1 == self.used_size1 {
-            self.used_size1 -= 1;
-        }
         let mut tmp: Option<Arc<Mutex<T1>>> = None;
         mem::swap(&mut tmp,
                   self.d1
                       .get_mut(id)
                       .expect(&format!("expected to find destroy_1 value {:?}", id)));
+
+        truncate_to_used_elements_if_removed_id_is_last(&mut self.d1, id, &mut self.used_size1);
     }
+
     pub fn destroy_2(&mut self, id: usize) {
-        if id + 1 == self.used_size2 {
-            self.used_size2 -= 1;
-        }
         let mut tmp: Option<Arc<Mutex<T2>>> = None;
         mem::swap(&mut tmp,
                   self.d2
                       .get_mut(id)
                       .expect(&format!("expected to find destroy_2 value {:?}", id)));
+
+        truncate_to_used_elements_if_removed_id_is_last(&mut self.d2, id, &mut self.used_size2);
     }
+}
+
+fn insert_into_vec<T>(data: &mut Vec<Option<Arc<Mutex<T>>>>,
+                      value: Arc<Mutex<T>>,
+                      used_size: &mut usize)
+                      -> usize {
+    for (i, item) in data.iter_mut().enumerate() {
+        if item.is_none() {
+            let mut tmp = Some(value);
+            mem::swap(&mut tmp, item);
+            if i + 1 > *used_size {
+                *used_size = i + 1;
+            }
+            return i;
+        }
+    }
+    let index = data.len();
+    data.push(Some(value));
+    if data.len() > *used_size {
+        *used_size = data.len();
+    }
+    index
+}
+
+fn truncate_to_used_elements_if_removed_id_is_last<T>(data: &mut Vec<Option<Arc<Mutex<T>>>>,
+                                                      removed_id: usize,
+                                                      used_size: &mut usize) {
+    if removed_id + 1 != *used_size {
+        return;
+    }
+    while *used_size > 0 {
+        *used_size -= 1;
+        if *used_size == 0 || !data[*used_size - 1].is_none() {
+            break;
+        }
+    }
+    data.truncate(*used_size);
 }
 
 pub struct Destructor<T1: Any, T2: Any> {
@@ -153,6 +164,7 @@ impl<T1: Any, T2: Any> Drop for Destructor<T1, T2> {
 
 #[cfg(test)]
 mod test {
+    use std::any::Any;
     use std::sync::{Arc, Mutex};
     use super::*;
 
@@ -160,9 +172,13 @@ mod test {
         data.iter().filter(|v| !v.is_none()).count()
     }
 
+    fn ic_with_val<A: Any, B: Any>() -> Inceptor<A, B> {
+        Inceptor::<A, B>::new(move |_a, _b| Ok(Some(Box::new(42))))
+    }
+
     #[test]
     fn should_register_and_destroy_first_arg() {
-        let mut ic = Inceptor::<i32, bool>::new(|_a, _b| Ok(Some(Box::new(42))));
+        let mut ic = ic_with_val::<i32, bool>();
         let (id, instances) = ic.incept_1(Arc::new(Mutex::new(11)))
             .expect("failed to incept first arg");
 
@@ -172,5 +188,52 @@ mod test {
         ic.destroy_1(id);
 
         assert_eq!(count_not_none(&mut ic.d1), 0);
+    }
+
+    #[test]
+    fn should_register_and_destroy_second_arg() {
+        let mut ic = ic_with_val::<i32, bool>();
+        let (id, instances) = ic.incept_2(Arc::new(Mutex::new(false)))
+            .expect("failed to incept first arg");
+
+        assert_eq!(instances.len(), 0);
+        assert_eq!(count_not_none(&mut ic.d2), 1);
+
+        ic.destroy_2(id);
+
+        assert_eq!(count_not_none(&mut ic.d2), 0);
+    }
+
+    #[test]
+    fn should_create_and_destroy_instances_for_all_existing_items() {
+        let mut ic = ic_with_val::<i32, bool>();
+        let mut value_num_3 = Arc::new(Mutex::new(3));
+        let (_, _) = ic.incept_1(Arc::new(Mutex::new(1))).unwrap();
+        let (_, _) = ic.incept_1(Arc::new(Mutex::new(2))).unwrap();
+        let (id3, _) = ic.incept_1(value_num_3.clone()).unwrap();
+        assert_eq!(count_not_none(&mut ic.d1), 3);
+
+        let (other_id, instances) = ic.incept_2(Arc::new(Mutex::new(false))).unwrap();
+        assert_eq!(instances.len(), 3);
+        assert_eq!(count_not_none(&mut ic.d2), 1);
+
+        // should not be possible to take out value from arc
+        value_num_3 = Arc::try_unwrap(value_num_3).unwrap_err();
+        // destroying parent should free up the instance that was created using it
+        ic.destroy_1(id3);
+        assert_eq!(count_not_none(&mut ic.d1), 2);
+        // should be possible to take out value from arc
+        {
+            let val = Arc::try_unwrap(value_num_3).expect("expected arc refcount 1");
+            assert_eq!(3, *val.lock().unwrap());
+        }
+        // memory should be freed
+        assert_eq!(ic.d1.len(), 2);
+
+        ic.destroy_2(other_id);
+        assert_eq!(count_not_none(&mut ic.d2), 0);
+
+        // memory should be freed
+        assert_eq!(ic.d2.len(), 0);
     }
 }
